b2da8cb2f7014d369d6e42696f4bdbb864427aae
tbowen8615 <tbowen8615@gmail.com>
2025-02-18 18:52:58 -0600

    CodePath Session 1

---
+++ Session1.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+
+print("I am learning datastructures")
+
+def hello():
+    hello = "hello universe"
+    print("Hello World!")
+    print(hello)
+    age = 25
+
+    print(f"I am {age}")
+
+    favs = ["books", "coffee", "gym", "coding!"]
+
+    print(f"my favorite things are {favs}")
+
+hello()
