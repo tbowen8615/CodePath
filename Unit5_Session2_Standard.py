6627940d8706420b562eab0ca9128c74e501b30e
tbowen8615 <tbowen8615@gmail.com>
2025-03-25 18:04:49 -0500

    CodePath Unit 5 and 6

---
+++ Unit5_Session2_Standard.py
@@ -0,0 +1,142 @@
+class Villager:
+    def __init__(self, name, species, catchphrase):
+        self.name = name
+        self.species = species
+        self.catchphrase = catchphrase
+        self.friends = []
+
+    def get_mutuals(self, new_contact):
+        mutuals = []
+
+        for friend in self.friends:
+            if friend in new_contact.friends:
+                mutuals.append(friend.name)
+
+        return mutuals
+
+"""
+bob = Villager("Bob", "Cat", "pthhhpth")
+marshal = Villager("Marshal", "Squirrel", "sulky")
+ankha = Villager("Ankha", "Cat", "me meow")
+fauna = Villager("Fauna", "Deer", "dearie")
+raymond = Villager("Raymond", "Cat", "crisp")
+stitches = Villager("Stitches", "Cub", "stuffin")
+
+bob.friends = [stitches, raymond, fauna]
+marshal.friends = [raymond, ankha, fauna]
+print(bob.get_mutuals(marshal))
+
+ankha.friends = [marshal]
+print(bob.get_mutuals(ankha))"
+"""
+
+"""
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+kk_slider = Node("K.K. Slider")
+harriet = Node("Harriet")
+saharah = Node("Saharah")
+isabelle = Node("Isabelle")
+
+kk_slider.next = harriet
+harriet.next = saharah
+saharah.next = isabelle
+
+print_linked_list(kk_slider)
+"""
+
+"""
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def add_first(head, task):
+    new_node = Node(task)
+    temp = head
+    head = new_node
+    new_node.next = temp
+    return head
+
+task_1 = Node("shake tree")
+task_2 = Node("dig fossils")
+task_3 = Node("catch bugs")
+task_1.next = task_2
+task_2.next = task_3
+
+# Linked List: shake tree -> dig fossils -> catch bugs
+print_linked_list(add_first(task_1, "check turnip prices"))"
+
+
+class Node:
+    def __init__(self, value, next=None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+def print_reverse(tail):
+    current = tail
+    while current:
+        print(current.value, end=" ")
+        current = current.prev
+
+isabelle = Node("Isabelle")
+kk_slider = Node("K.K. Slider")
+saharah = Node("Saharah")
+isabelle.next = kk_slider
+kk_slider.next = saharah
+saharah.prev = kk_slider
+kk_slider.prev = isabelle
+
+# Linked List: Isabelle <-> K.K. Slider <-> Saharah
+print_reverse(saharah)"
+"""
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def remove_nth_from_end(head, n):
+    count = 0
+    current = head
+    
+    while current:
+        count += 1
+        current = current.next
+    
+    
+    pass
+
+head1 = Node("apple", Node("cherry", Node("orange", Node("peach", Node("pear")))))
+head2 = Node("Rainbow Trout", Node("Ray"))
+head3 = Node("Rainbow Stag")
+
+
+print_linked_list(remove_nth_from_end(head1, 2))
+print_linked_list(remove_nth_from_end(head2, 1))
+print_linked_list(remove_nth_from_end(head3, 1))
